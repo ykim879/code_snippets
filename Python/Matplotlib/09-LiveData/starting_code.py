b4fa00cfae34bb6b12c51fde3fc36603e2cb62c1
Yiyeon Kim <59812671+ykim879@users.noreply.github.com>
2020-07-11 21:05:03 -0700

    Part 10

--- Python/Matplotlib/09-LiveData/starting_code.py
+++ Python/Matplotlib/09-LiveData/starting_code.py
@@ -13,20 +13,32 @@ y_vals = []
 
 index = count()
 
-def animate(i):
-    x_vals.append(next(index))
-    y_vals.append(random.randint(0, 5))
+# def animate(i):
+#     x_vals.append(next(index))
+#     y_vals.append(random.randint(0, 5))
 
-    plt.cla()
-    plt.plot(x_vals, y_vals, colors = )
+#     plt.cla()
+#     plt.plot(x_vals, y_vals, colors = )
+
+def animate(i):
+	data = pd.read_csv('data.csv')
+	x = data['x_value']
+	y1 = data['total_1']
+	y2 = data['total_2']
+	median1 = y1.median()
+	median2 = y2.median()
+
+	plt.cla()
+	plt.plot(x, y1, label = "Channel 1")
+	plt.plot(x, y2, label = "Channel 2")
+	plt.axhline(median1, color = "#000000", label = "Channel 1 median")
+	plt.axhline(median2, color = "#111111", label = "Channel 2 median")
+	plt.legend(loc = "upper right") #location for constant location
+
+	plt.tight_layout()
 
 ani = FuncAnimation(plt.gcf(), animate, interval = 1000)
 
-plt.tight_layout()
 plt.show()
 
 
-# data = pd.read_csv('data.csv')
-# x = data['x_value']
-# y1 = data['total_1']
-# y2 = data['total_2']
--- Python/Matplotlib/10-Subplots/starting_code.py
+++ Python/Matplotlib/10-Subplots/starting_code.py
@@ -10,17 +10,25 @@ dev_salaries = data['All_Devs']
 py_salaries = data['Python']
 js_salaries = data['JavaScript']
 
-plt.plot(ages, py_salaries, label='Python')
-plt.plot(ages, js_salaries, label='JavaScript')
+fig, (ax1, ax2) = plt.subplots(nrows = 2, ncols = 1, sharex = True)
 
-plt.plot(ages, dev_salaries, color='#444444',
+
+
+ax2.plot(ages, dev_salaries, color='#444444',
          linestyle='--', label='All Devs')
 
-plt.legend()
 
-plt.title('Median Salary (USD) by Age')
-plt.xlabel('Ages')
-plt.ylabel('Median Salary (USD)')
+ax1.plot(ages, py_salaries, label='Python')
+ax1.plot(ages, js_salaries, label='JavaScript')
+
+ax1.legend()
+ax2.legend()
+
+ax1.set_title('Median Salary (USD) by Age')
+ax1.set_ylabel('Median Salary (USD)')
+
+ax2.set_xlabel('Ages')
+ax2.set_ylabel('Median Salary (USD)')
 
 plt.tight_layout()
 
