6021cd07c94894c7407c36cc7c733466febf6249
Yiyeon Kim <59812671+ykim879@users.noreply.github.com>
2020-07-13 21:59:06 -0700

    barh + pie chart

--- Python/Matplotlib/03-PieCharts/finished_code.py
+++ Python/Matplotlib/03-PieCharts/finished_code.py
@@ -6,10 +6,20 @@ slices = [59219, 55466, 47544, 36443, 35917]
 labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java']
 explode = [0, 0, 0, 0.1, 0]
 
-plt.pie(slices, labels=labels, explode=explode, shadow=True,
+fig, (ax1, ax2) = plt.subplots(nrows = 2, ncols = 1, sharex = False)
+
+ax1.pie(slices, labels=labels, explode=explode, shadow=True,
         startangle=90, autopct='%1.1f%%',
         wedgeprops={'edgecolor': 'black'})
 
-plt.title("My Awesome Pie Chart")
+s_slices = []
+for s in slices:
+	s_slices.append(str(s))
+
+ax2.barh(labels, s_slices)
+
+ax1.set_title("Frequency of Using Programming Languages")
 plt.tight_layout()
 plt.show()
+
+fig.savefig("finished_code.png")
