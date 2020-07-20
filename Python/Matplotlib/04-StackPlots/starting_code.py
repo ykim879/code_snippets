eb39c17ccd76d31ee2dc9639de5c2537b45be8a0
Yiyeon Kim <59812671+ykim879@users.noreply.github.com>
2020-07-19 20:25:49 -0700

    update stackplot

--- Python/Matplotlib/04-StackPlots/starting_code.py
+++ Python/Matplotlib/04-StackPlots/starting_code.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 
 plt.style.use("fivethirtyeight")
 
@@ -9,11 +10,31 @@ player1 = [1, 2, 3, 3, 4, 4, 4, 4, 5]
 player2 = [1, 1, 1, 1, 2, 2, 2, 3, 4]
 player3 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
 
-plt.pie([1, 1, 1], labels=["Player 1", "Player2", "Player3"])
+labels=["Player 1", "Player2", "Player3"]
 
-plt.title("My Awesome Stack Plot")
+fig, (ax1, ax2, ax3) = plt.subplots(nrows = 3, ncols = 1)
+
+ax1.pie([1, 1, 1], labels = labels)
+ax1.set_title("Data on Pie Chart")
+
+ax2.stackplot(minutes, player1, player2, player3, labels = labels)
+ax2.legend(loc = (0,1))
+ax2.set_title("Data on Stack Plot")
+ax2.set_ylabel("shots per a game")
+ax2.set_xlabel("minutes")
+
+data = {
+	"player1" : player1,
+	"player2" : player2,
+	"player3" : player3
+}
+df = pd.DataFrame(data)
+
+ax3.barh(labels, [df["player1"].median(), df["player2"].median(), df["player3"].median()])
+ax3.set_xlabel("average shot for each player")
 plt.tight_layout()
 plt.show()
+plt.savefig("my_final.png")
 
 # Colors:
 # Blue = #008fd5
